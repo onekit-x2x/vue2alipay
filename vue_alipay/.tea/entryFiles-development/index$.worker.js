f04eec441fe93038f1c461222e257f84d39f4844
gongyuan2 <68527713+gongyuan2@users.noreply.github.com>
2020-09-11 17:19:46 +0800

    v.01

--- vue_alipay/.tea/entryFiles-development/index$.worker.js
+++ vue_alipay/.tea/entryFiles-development/index$.worker.js
@@ -5,19 +5,22 @@ self.__appxInited = 1;
 require('./config$');
 
 
-  var AFAppX = self.AFAppX.getAppContext
-    ? self.AFAppX.getAppContext().AFAppX
-    : self.AFAppX;
-  self.getCurrentPages = AFAppX.getCurrentPages;
-  self.getApp = AFAppX.getApp;
-  self.Page = AFAppX.Page;
-  self.App = AFAppX.App;
-  self.my = AFAppX.bridge || AFAppX.abridge;
-  self.abridge = self.my;
-  self.Component = AFAppX.WorkerComponent || function(){};
-  self.$global = AFAppX.$global;
-  self.requirePlugin = AFAppX.requirePlugin;
-          
+      if( navigator.userAgent && (navigator.userAgent.indexOf('LyraVM') > 0 || navigator.userAgent.indexOf('AlipayIDE') > 0) ) {
+        var AFAppX = self.AFAppX.getAppContext ? self.AFAppX.getAppContext().AFAppX : self.AFAppX;
+      } else {
+        importScripts('https://appx/af-appx.worker.min.js');
+        var AFAppX = self.AFAppX;
+      }
+      self.getCurrentPages = AFAppX.getCurrentPages;
+      self.getApp = AFAppX.getApp;
+      self.Page = AFAppX.Page;
+      self.App = AFAppX.App;
+      self.my = AFAppX.bridge || AFAppX.abridge;
+      self.abridge = self.my;
+      self.Component = AFAppX.WorkerComponent || function(){};
+      self.$global = AFAppX.$global;
+      self.requirePlugin = AFAppX.requirePlugin;
+    
 
 if(AFAppX.registerApp) {
   AFAppX.registerApp({
@@ -29,6 +32,7 @@ if(AFAppX.registerApp) {
 
 function success() {
 require('../../app');
+require('../../gong/gong?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../pages/index/index?hash=5158fa18297db3fbaac119609b168d20fcdf1eea');
 }
 self.bootstrapApp ? self.bootstrapApp({ success }) : success();
